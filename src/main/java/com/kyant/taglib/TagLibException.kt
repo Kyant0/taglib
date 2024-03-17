88d34a8ea96a1d67540642db2232b282ee07672b
Kyant0 <kyant2021@outlook.com>
2024-03-17 13:43:35 +0800

    Transform JNI exception into Java Exception Added TagLibException

--- src/main/java/com/kyant/taglib/TagLibException.kt
+++ src/main/java/com/kyant/taglib/TagLibException.kt
@@ -1,3 +1,6 @@
 package com.kyant.taglib
 
+/**
+ * An exception that is thrown when an error occurs in the TagLib library.
+ */
 public class TagLibException(message: String) : RuntimeException(message)
